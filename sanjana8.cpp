0b7e4676e54c49f493906b21ed053c11eb8e919b
11249a288-jpg <11249a288@kanchiuniv.ac.in>
2025-11-10 13:04:56 +0530

    Add files via upload

---
+++ sanjana8.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+template <typename T >
+T add(Ta,Tb)
+{
+    return a+b;
+}
+intmain() {
+    cout<<"sum(int):"<<add(10,20)<<endl;
+    cout<<"sum(float):"<<add(2.5,3.5)<<endl;
+    return 0;
+}
+
